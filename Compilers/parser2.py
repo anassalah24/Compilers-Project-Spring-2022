389f5026f73c2517b3a765ef71bb9ce14fac8e04
Anas Salah <68183749+anassalah24@users.noreply.github.com>
2022-05-25 16:54:02 +0200

    Add files via upload

--- Compilers/parser2.py
+++ Compilers/parser2.py
@@ -549,6 +549,6 @@ def main(tokens):
 
 
 
-if __name__ == '__main__':
-
-    main("if number then id := number ; end")
+# if __name__ == '__main__':
+#
+#     main("if number then id := number ; end")
